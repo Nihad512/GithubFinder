dbfdf3daceaa91479126e3b0173dc21743fbcfca
nihad512 <nihad0babayev@gmail.com>
2023-07-05 15:08:51 +0200

    first commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResult from "../users/UserResult";
+import UserSearch from "../users/UserSearch";
+function Home() {
+    return (
+        <div>
+            <UserSearch/>
+            <UserResult/>
+        </div>
+    );
+}
+
+export default Home;
